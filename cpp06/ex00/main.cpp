b1aa2ad8cd48d8f5e8ff245164c3e482209e6d2f
William Rocha <williamrocha@wsl.com>
2025-02-03 10:48:52 +0000

    f*ck  that all

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,9 +1,11 @@
 #include "ScalarConverter.hpp"
 
-int main(int argc, char **argv) {
-  if (argc != 2)
-    std::cout << "input invalid!" << std::endl;
-  else
-    ScalarConverter::convert(argv[1]);
+int main(int ac, char **av) {
+  if (ac != 2) {
+    std::cout << "Error: Invalid number of arguments." << std::endl;
+    std::cout << "Usage: ./convert <literal>" << std::endl;
+    return 1;
+  } else
+    ScalarConverter::convert(av[1]);
   return (0);
 }
